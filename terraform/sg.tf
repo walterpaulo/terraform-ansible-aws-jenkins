789f4729556bf684dc2bb8fdc4e26737e01bcacd
walterpaulo <walter0paulo@hotmail.com>
2022-04-24 00:00:41 -0300

    fix: added ip

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "liberar_portas" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
       ipv6_cidr_blocks = ["::/0"]
       description      = "Servico ssh"
       prefix_list_ids  = null
@@ -32,7 +32,7 @@ resource "aws_security_group" "liberar_portas" {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       description      = "Liberacao geral na parte interna"
       prefix_list_ids  = null
